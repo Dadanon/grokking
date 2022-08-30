3b288389d165044db7b93f29dacd516e6ea24493
Dadanon <mar4ello19922@gmail.com>
2022-08-30 16:42:17 +0300

    Try to make sort more effective... But didn't success :(

--- Chapter4 - Fast sort/fast_sort.py
+++ Chapter4 - Fast sort/fast_sort.py
@@ -1,5 +1,6 @@
 from datetime import datetime
-from random import randint
+import random
+import timeit
 
 
 def quick_sort(array):
@@ -12,10 +13,33 @@ def quick_sort(array):
         return quick_sort(less) + [pivot] + quick_sort(greater)
 
 
-arr = [randint(1, 1000000) for i in range(1000000)]
-time_now = datetime.now()
-quick_sort(arr)
-print(f"Time is: {datetime.now() - time_now}")
-time_now_2 = datetime.now()
-arr.sort()
-print(f"Time is: {datetime.now() - time_now_2}")
+def quick_sort_plus(array):
+    if len(array) < 2:
+        return array
+    if len(array) == 3:
+        pivot = array[0]
+        less = [i for i in array[1:] if i <= pivot]
+        greater = [i for i in array[1:] if i > pivot]
+        return quick_sort_plus(less) + [pivot] + quick_sort_plus(greater)
+    else:
+        middle = len(array) // 2
+        pivot1 = array[0]
+        pivot2 = array[-1]
+        less1 = [i for i in array[1:middle] if i <= pivot1]
+        greater1 = [i for i in array[1:middle] if i > pivot1]
+        less2 = [i for i in array[middle:-1] if i <= pivot2]
+        greater2 = [i for i in array[middle:-1] if i > pivot2]
+        return quick_sort_plus(less1) + [pivot1] + quick_sort_plus(greater1) + \
+               quick_sort_plus(less2) + [pivot2] + quick_sort_plus(greater2)
+
+
+arr = [random.randint(1, 1000000) for i in range(50000)]
+
+#time_now = datetime.now()
+#quick_sort(arr)
+#print(f"Time is: {datetime.now() - time_now}")
+#time_now_2 = datetime.now()
+#quick_sort_plus(arr)
+#print("Time plus is: ")
+# print(timeit.timeit("quick_sort(arr)", number=1000, globals=globals()))
+print(timeit.timeit("quick_sort_plus(arr)", number=20, globals=globals()))
---
+++ Chapter5 - Hashtables/Using of hashtables.md
@@ -0,0 +1,33 @@
+**Using of hashtables**
+
+- _Using hashtables for search_
+
+
+    phone_book = {}
+    
+    phone_book['Jenny'] = 8960435
+    
+    phone_book['Urgency'] = 112
+    
+    print(phone_book['Jenny']) # 8960435
+
+Also IP adresses in WEB
+
+- _Elimination of duplicates_
+
+
+    voted = {}
+    
+    def can_vote(person):
+
+        if voted.get(person):
+            print("Kick them out!")
+        else:
+            print("Let them vote!")
+            voted[person] = True
+        
+- _Caching_
+
+If web-page is not in cash -> do some server work
+
+Else -> load cached page
